063f5b9a672de4ac88fe6ccd92013aeec5c86fca
Stefania Occhilupo <38841416+Swolfeyes@users.noreply.github.com>
2019-12-19 10:39:31 +0000

    test change

--- app.js
+++ app.js
@@ -1,12 +1,3 @@
-const someJsCode = () => {
-  console.log('some code')
-}
-
-someJsCode()
-console.log('ciaooooo')
-
 const addingAnotherFun = () => {
   console.log('ciao ancora!!');
 }
-
-making a change that is not meant to be commited here
